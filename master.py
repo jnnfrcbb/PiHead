47ec1e0c470bd1ef4fec43e9f037628b7b63a7a2
jnnfrcbb <89925918+jnnfrcbb@users.noreply.github.com>
2021-09-03 17:03:24 +0100

    enter

--- master.py
+++ master.py
@@ -3,11 +3,58 @@
 #can easily change the master script list and install to / update from pi using git
 #add reference in /etc/rc.local
 
-import RPi.GPIO as GPIO # import our GPIO module
-import time
-from subprocess import call
 import os
 
 #CONTROLLER_SERVICE
-os.system ("controller_service \home\pi\PiHead\volume_encoder.ini")     #ENC1
-os.system ("controller_service \home\pi\PiHead\playback_encoder.ini")   #ENC2
+os.system ("controller_service /home/pi/PiHead/volume_encoder.ini")     #ENC1
+os.system ("controller_service /home/pi/PiHead/playback_encoder.ini")   #ENC2
+
+#CARPIHAT
+##CarPiHat safeshutdown
+import CarPiHat
+dtoverlay=gpio-poweroff,gpiopin=25,active_low
+
+with open("/boot/config.txt", "a+") as myFile:
+    if len(myFile) > 0:
+            myFile.write("\n")
+    if not "dtoverlay=gpio-poweroff,gpiopin=25,active_low" in myFile:
+        myFile.write ("dtoverlay=gpio-poweroff,gpiopin=25,active_low")
+    myFile.close()
+
+##CarPiHat CanBus interface
+os.system ("/sbin/ip link set can0 up type can bitrate 100000")
+
+with open("/boot/config.txt", "a+") as myFile:
+    if len(myFile) > 0:
+            myFile.write("\n")
+    if not "dtparam=spi=on" in myFile:
+        myFile.write ("dtparam=spi=on \n")
+    if not "dtoverlay=mcp2515-can0,oscillator=8000000,interrupt=23" in myFile:
+        myFile.write("dtoverlay=mcp2515-can0,oscillator=8000000,interrupt=23 \n")    
+    if not "dtoverlay=spi-bcm2835-overlay" in myFile:
+        myFile.write("dtoverlay=spi-bcm2835-overlay")
+    #if not "dtoverlay=spi0-cs,cs1_pin=24" in myFile:    #uncomment if reverse doesn't work when using CAN bus
+    #    myFile.write("\ndtoverlay=spi0-cs,cs1_pin=24")
+    myFile.close()
+
+with open("/etc/rc.local", "a+") as myFile:
+    if len(myFile) > 0:
+            myFile.write("\n")
+    if not "/sbin/ip link set can0 up type can bitrate 100000" in myFile:
+        myFile.write ("/sbin/ip link set can0 up type can bitrate 100000")
+    myFile.close()
+
+##CarPiHat real time clock
+with open("/etc/rc.local", "a+") as myFile:
+    if len(myFile) > 0:
+            myFile.write("\n")
+    if not "echo ds1307 0x68 > /sys/class/i2c-adapter/i2c-1/new_device hwclock -s" in myFile:
+        myFile.write ("echo ds1307 0x68 > /sys/class/i2c-adapter/i2c-1/new_device hwclock -s")
+    myFile.close()
+
+with open("/etc/modules", "a+") as myFile:
+    if len(myFile) > 0:
+            myFile.write("\n")
+    if not "rtc-ds1307" in myFile:
+        myFile.write ("rtc-ds1307")
+    myFile.close()
