0180e9e94b01575bc0cf9417ae26fda9746740fd
jnnfrcbb <89925918+jnnfrcbb@users.noreply.github.com>
2021-09-03 17:31:02 +0100

    enter

--- master.py
+++ master.py
@@ -5,71 +5,81 @@
 
 import os
 
+
+ 
 #CONTROLLER_SERVICE
 with open("/etc/xdg/lxsession/LXDE-pi/autostart", "a+") as myFile:
-    if len(myFile)> 0:
+    fileData = myFile.read()
+    if len(fileData)> 0:
         myFile.write("\n")
-    if not "controller_service /home/pi/PiHead/volume_encoder.ini" in myFile:
+    if not "controller_service /home/pi/PiHead/volume_encoder.ini" in fileData:
         myFile.write("controller_service /home/pi/PiHead/volume_encoder.ini")       #ENC1
-    if not "controller_service /home/pi/PiHead/playback_encoder.ini" in myFile:
+    if not "controller_service /home/pi/PiHead/playback_encoder.ini" in fileData:
         myFile.write("controller_service /home/pi/PiHead/playback_encoder.ini")     #ENC2
     myFile.close()
+
+
 
 #CARPIHAT
 ##CarPiHat safeshutdown
 import CarPiHat
 
 with open("/boot/config.txt", "a+") as myFile:
-    if len(myFile) > 0:
+    fileData = myFile.read()
+    if len(fileData) > 0:
             myFile.write("\n")
-    if not "#CarPiHat" in myFile:
+    if not "#CarPiHat" in fileData:
         myFile.write ("#CarPiHat \n")
-    if not "dtoverlay=gpio-poweroff,gpiopin=25,active_low" in myFile:
+    if not "dtoverlay=gpio-poweroff,gpiopin=25,active_low" in fileData:
         myFile.write ("dtoverlay=gpio-poweroff,gpiopin=25,active_low")
     myFile.close()
 
 ##CarPiHat CanBus interface
 os.system ("/sbin/ip link set can0 up type can bitrate 100000")
 
-with open("/boot/config.txt", "a+") as myFile:
-    if len(myFile) > 0:
+with open("/boot/config.txt", "a+") as myFile:    
+    fileData = myFile.read()
+    if len(fileData) > 0:
             myFile.write("\n")
-    if not "#CarPiHat" in myFile:
+    if not "#CarPiHat" in fileData:
         myFile.write ("#CarPiHat \n")
-    if not "dtparam=spi=on" in myFile:
+    if not "dtparam=spi=on" in fileData:
         myFile.write ("dtparam=spi=on \n")
-    if not "dtoverlay=mcp2515-can0,oscillator=8000000,interrupt=23" in myFile:
+    if not "dtoverlay=mcp2515-can0,oscillator=8000000,interrupt=23" in fileData:
         myFile.write("dtoverlay=mcp2515-can0,oscillator=8000000,interrupt=23 \n")    
-    if not "dtoverlay=spi-bcm2835-overlay" in myFile:
+    if not "dtoverlay=spi-bcm2835-overlay" in fileData:
         myFile.write("dtoverlay=spi-bcm2835-overlay")
-    #if not "dtoverlay=spi0-cs,cs1_pin=24" in myFile:    #uncomment if reverse doesn't work when using CAN bus
+    #if not "dtoverlay=spi0-cs,cs1_pin=24" in fileData:    #uncomment if reverse doesn't work when using CAN bus
     #    myFile.write("\ndtoverlay=spi0-cs,cs1_pin=24")
     myFile.close()
 
 with open("/etc/rc.local", "a+") as myFile:
-    if len(myFile) > 0:
+    fileData = myFile.read()
+    if len(fileData) > 0:
             myFile.write("\n")            
-    if not "#CarPiHat" in myFile:
+    if not "#CarPiHat" in fileData:
         myFile.write ("#CarPiHat \n")
-    if not "/sbin/ip link set can0 up type can bitrate 100000" in myFile:
+    if not "/sbin/ip link set can0 up type can bitrate 100000" in fileData:
         myFile.write ("/sbin/ip link set can0 up type can bitrate 100000")
     myFile.close()
 
 ##CarPiHat real time clock
 with open("/etc/rc.local", "a+") as myFile:
-    if len(myFile) > 0:
+    fileData = myFile.read()
+    if len(fileData) > 0:
             myFile.write("\n")
-    if not "#CarPiHat" in myFile:
+    if not "#CarPiHat" in fileData:
         myFile.write ("#CarPiHat \n")
-    if not "echo ds1307 0x68 > /sys/class/i2c-adapter/i2c-1/new_device hwclock -s" in myFile:
+    if not "echo ds1307 0x68 > /sys/class/i2c-adapter/i2c-1/new_device hwclock -s" in fileData:
         myFile.write ("echo ds1307 0x68 > /sys/class/i2c-adapter/i2c-1/new_device hwclock -s")
     myFile.close()
 
 with open("/etc/modules", "a+") as myFile:
-    if len(myFile) > 0:
+    fileData = myFile.read()
+    if len(fileData) > 0:
             myFile.write("\n")
-    if not "#CarPiHat" in myFile:
+    if not "#CarPiHat" in fileData:
         myFile.write ("#CarPiHat \n")
-    if not "rtc-ds1307" in myFile:
+    if not "rtc-ds1307" in fileData:
         myFile.write ("rtc-ds1307")
     myFile.close()
