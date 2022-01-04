67b9d1ccdb65d810b8676e1535432958438fc339
jnnfrcbb <89925918+jnnfrcbb@users.noreply.github.com>
2022-01-04 23:48:49 +0000

    enter

--- rgb_strip.py
+++ rgb_strip.py
@@ -1,6 +1,8 @@
 import board
 import neopixel
 
+##neopixel library: "sudo pip3 install rpi_ws281x adafruit-circuitpython-neopixel"
+
 pixelCount = 8
 
 pixels = neopixel.NeoPixel(board.D18, 8) ##number = pixel count
