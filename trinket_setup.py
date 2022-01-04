ba1557a0adca108fc3c5a1347b2150bdcccf2eb9
jnnfrcbb <89925918+jnnfrcbb@users.noreply.github.com>
2022-01-04 23:06:50 +0000

    enter

--- trinket_setup.py
+++ trinket_setup.py
@@ -6,9 +6,17 @@ source = "/home/pi/PiHead/main.py"
 destinationFolder = "/media/pi/CIRCUITPY/"
 destinationFile = "main.py"
 
-fileCopied = False
-while fileCopied == False:
-    if os.path.exists(destinationFolder):
-        shutil.copy2(source, destinationFolder + destinationFile)
-        fileCopied = True
+##code option 1
+while not os.path.exists(destinationFolder):
     time.sleep(0.25)
+shutil.copy2(source, destinationFolder + destinationFile)
+
+
+##code option 2
+#fileCopied = False
+#while fileCopied == False:
+#    if os.path.exists(destinationFolder):
+#        ret = shutil.copy2(source, destinationFolder + destinationFile)
+#        print(ret)
+#        fileCopied = True
+#    time.sleep(0.25)
