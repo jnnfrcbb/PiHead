7ae7762592425cf3eaa2271c40e41d18ee8d67af
jnnfrcbb <89925918+jnnfrcbb@users.noreply.github.com>
2022-01-07 19:11:09 +0000

    enter

--- display_power.py
+++ display_power.py
@@ -5,7 +5,7 @@
 #os.system("sudo bash -c echo 0 > /sys/class/backlight/rpi_backlight/bl_power")
 
 backlight = open('/sys/class/backlight/rpi_backlight/bl_power', 'w')
-print (backlight)
+print (backlight.read)
 #backlight.write('1') # turn on
 #backlight.write('0') # turn off
 
