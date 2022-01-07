d99cc3c56b924c71b219e6f217771fdd677383c5
jnnfrcbb <89925918+jnnfrcbb@users.noreply.github.com>
2022-01-07 19:47:10 +0000

    enter

--- bl_toggle.py
+++ bl_toggle.py
@@ -2,6 +2,8 @@ bl_status = open("/sys/class/backlight/rpi_backlight/bl_power", "r")
 bl_Status = (bl_status.read())
 bl_status.close()
 
+print(bl_Status)
+
 if bl_Status == 0:
     backlight = open('/sys/class/backlight/rpi_backlight/bl_power', 'w')
     backlight.write('1') # turn on
