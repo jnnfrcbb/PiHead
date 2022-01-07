085d36c232adea2caae5bbcaafaa710702275c45
jnnfrcbb <89925918+jnnfrcbb@users.noreply.github.com>
2022-01-07 19:08:56 +0000

    enter

--- display_power.py
+++ display_power.py
@@ -1,10 +1,16 @@
-import os
+#import os
 
 
 #turn screen on:
-os.system("sudo bash -c echo 0 > /sys/class/backlight/rpi_backlight/bl_power")
+#os.system("sudo bash -c echo 0 > /sys/class/backlight/rpi_backlight/bl_power")
+
+backlight = open('/sys/class/backlight/rpi_backlight/bl_power', 'w')
+print (backlight)
+#backlight.write('1') # turn on
+#backlight.write('0') # turn off
+
 
 #turn screen off:
-os.system("sudo bash -c echo 255 > /sys/class/backlight/rpi_backlight/bl_power")
+#os.system("sudo bash -c echo 255 > /sys/class/backlight/rpi_backlight/bl_power")
 
 #todo: read current state pause; playback when screen off; bind hotkey to run script
