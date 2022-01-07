f3e5b1c8628821c3efa5e6bd3d40f5399042dd40
jnnfrcbb <89925918+jnnfrcbb@users.noreply.github.com>
2022-01-07 23:52:53 +0000

    enter

--- master.py
+++ master.py
@@ -76,7 +76,7 @@ m = appendString("/etc/xdg/lxsession/LXDE-pi/autostart","controller_service /hom
 ##############################
 ## HOTKEY FOR DISPLAY POWER ##
 ##############################
-#m = replaceString("/etc/xdg/openbox/lxde-pi-rc.xml","<chainQuitKey>C-g</chainQuitKey>","<chainQuitKey>C-g</chainQuitKey>\r    <keybind key=""C-F12""><action name=""bl_toggle""><command>bl_toggle.sh</command></action></keybind>")
+m = replaceString("/etc/xdg/openbox/lxde-pi-rc.xml","<chainQuitKey>C-g</chainQuitKey>","<chainQuitKey>C-g</chainQuitKey>\r    <keybind key=""C-F12"">\r      <action name=""bl_toggle"">\r        <command>bl_toggle.sh</command>\r      </action>\r    </keybind>")
 
 
 ###################
