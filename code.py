3e2e24eab02b24c1d5d59be39bc1828a972291db
jnnfrcbb <89925918+jnnfrcbb@users.noreply.github.com>
2022-01-10 18:36:08 +0000

    enter

--- code.py
+++ code.py
@@ -58,58 +58,58 @@ while True:
 
   bButton = False
 
-  if VD1 > 1:
-    #print("SHIFT: FALSE")
-    if VD0 < 0.1:
-      print("BUTTON: OFF")
-      kbd.send(Keycode.H)  #HOME
-      bButton = True
-    elif VD0 < 0.7:
-      print("BUTTON: SOURCE")
-      kbd.send(Keycode.M)  #VOICE
-      bButton = True
-    elif VD0 < 1.1:
-      print("BUTTON: ATT")
-      kbd.send(Keycode.CONTROL, Keycode.F3) #SWITCH MODE      
-      bButton = True
-    elif VD0 < 1.4:
-      print("BUTTON: LIST")
-      kbd.send(Keycode.CONTROL, Keycode.F11) #TOGGLE MUTE
-      bButton = True
-    elif VD0 < 1.65:
-      print("BUTTON: SEEK+")
-      kbd.send(Keycode.N)  #NEXT TRACK
-      bButton = True
-    elif VD0 < 1.9:
-      print("BUTTON: SEEK-")
-      kbd.send(Keycode.V)  #PREVIOUS TRACK
-      bButton = True
-    elif VD0 < 2.15:
-      print("BUTTON: VOL+")
-      kbd.send(Keycode.F8) #VOLUME up
-      bButton = True
-    elif VD0 < 2.4:
-      print("BUTTON: VOL-")
-      kbd.send(Keycode.F7) #VOLUME DOWN
-      bButton = True
-    elif VD0 < 2.6:
-      print("BUTTON: SEL")
-      kbd.send(Keycode.B)  #PLAY/PAUSE
-      bButton = True
-    elif VD0 < 2.8:
-      print("BUTTON: MODE")
-      kbd.send(Keycode.CONTROL, Keycode.ALT, Keycode.B) #SCREEN POWER TOGGLE
-      bButton = True
-  elif VD1 < 1:
-    #print("SHIFT: TRUE")
-    if VD0 < 1.6:
-      print("BUTTON: SHIFTUP")
-      kbd.send(Keycode.J)  #LAUNCH MEDIA
-      bButton = True
-    elif VD0 < 1.9:
-      print("BUTTON: SHIFTDOWN")
-      kbd.send(Keycode.F) #LAUNCH NAVIGATION
-      bButton = True
+#  if VD1 > 1:
+#    #print("SHIFT: FALSE")
+#    if VD0 < 0.1:
+#      print("BUTTON: OFF")
+#      kbd.send(Keycode.H)  #HOME
+#      bButton = True
+#    elif VD0 < 0.7:
+#      print("BUTTON: SOURCE")
+#      kbd.send(Keycode.M)  #VOICE
+#      bButton = True
+#    elif VD0 < 1.1:
+#      print("BUTTON: ATT")
+#      kbd.send(Keycode.CONTROL, Keycode.F3) #SWITCH MODE      
+#      bButton = True
+#    elif VD0 < 1.4:
+#      print("BUTTON: LIST")
+#      kbd.send(Keycode.CONTROL, Keycode.F11) #TOGGLE MUTE
+#      bButton = True
+#    elif VD0 < 1.65:
+#      print("BUTTON: SEEK+")
+#      kbd.send(Keycode.N)  #NEXT TRACK
+#      bButton = True
+#    elif VD0 < 1.9:
+#      print("BUTTON: SEEK-")
+#      kbd.send(Keycode.V)  #PREVIOUS TRACK
+#      bButton = True
+#    elif VD0 < 2.15:
+#      print("BUTTON: VOL+")
+#      kbd.send(Keycode.F8) #VOLUME up
+#      bButton = True
+#    elif VD0 < 2.4:
+#      print("BUTTON: VOL-")
+#      kbd.send(Keycode.F7) #VOLUME DOWN
+#      bButton = True
+#    elif VD0 < 2.6:
+#      print("BUTTON: SEL")
+#      kbd.send(Keycode.B)  #PLAY/PAUSE
+#      bButton = True
+#    elif VD0 < 2.8:
+#      print("BUTTON: MODE")
+#      kbd.send(Keycode.CONTROL, Keycode.ALT, Keycode.B) #SCREEN POWER TOGGLE
+#      bButton = True
+#  elif VD1 < 1:
+#    #print("SHIFT: TRUE")
+#    if VD0 < 1.6:
+#      print("BUTTON: SHIFTUP")
+#      kbd.send(Keycode.J)  #LAUNCH MEDIA
+#      bButton = True
+#    elif VD0 < 1.9:
+#      print("BUTTON: SHIFTDOWN")
+#      kbd.send(Keycode.F) #LAUNCH NAVIGATION
+#      bButton = True
 
 #  if bButton == True:
 #    led[0] = (Rgb, rGb, rgB)
