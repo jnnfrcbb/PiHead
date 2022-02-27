c38e631c97c5fab432f5ccfdb37b4ecab2870ee8
jnnfrcbb <89925918+jnnfrcbb@users.noreply.github.com>
2022-02-27 16:54:33 +0000

    enter

--- master.py
+++ master.py
@@ -121,27 +121,27 @@ sourceFolder = "/home/pi/PiHead/LightSensor/"
 destFolder = "/opt/lightsensor/"
 files=["lightsensor_default_env.sh", "lightsensor_env.sh", "lightsensor.service", "service_lightsensor.py"]
 
-if os.path.isdir(destFolder):
-    os.system("sudo chmod -R a+rw " + destFolder)
-    os.system("sudo rm -rf " + destFolder + "*")
-    bNew = False
-else:
-    os.system("sudo mkdir -p "+ destFolder)
-    os.system("sudo chmod a+rw " + destFolder)
-    bNew = True
-    
-for i in files:
-    os.system("sudo cp -f " + sourceFolder + files[i] + " " + destFolder)
-    os.system("sudo chmod a+rwx " + destFolder + files[i])
-
-os.system("sudo cp -f " + destFolder + "lightsensor.service /etc/systemd/system")
-
-if bNew == True:
-    os.system("sudo systemctl enable lightsensor.service")
-    os.system("sudo systemctl start lightsensor.service")
-else:
-    os.system("sudo systemctl daemon-reload")
-    os.system("sudo systemctl restart lightsensor.service")
+#if os.path.isdir(destFolder):
+#    os.system("sudo chmod -R a+rw " + destFolder)
+#    os.system("sudo rm -rf " + destFolder + "*")
+#    bNew = False
+#else:
+#    os.system("sudo mkdir -p "+ destFolder)
+#    os.system("sudo chmod a+rw " + destFolder)
+#    bNew = True
+#    
+#for i in files:
+#    os.system("sudo cp -f " + sourceFolder + files[i] + " " + destFolder)
+#    os.system("sudo chmod a+rwx " + destFolder + files[i])
+#
+#os.system("sudo cp -f " + destFolder + "lightsensor.service /etc/systemd/system")
+#
+#if bNew == True:
+#    os.system("sudo systemctl enable lightsensor.service")
+#    os.system("sudo systemctl start lightsensor.service")
+#else:
+#    os.system("sudo systemctl daemon-reload")
+#    os.system("sudo systemctl restart lightsensor.service")
 
 
 ##############
