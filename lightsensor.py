96fc98f120b6d9598989af95c76312f87cf9d058
jnnfrcbb <89925918+jnnfrcbb@users.noreply.github.com>
2022-12-03 19:01:40 +0000

    enter

--- lightsensor.py
+++ lightsensor.py
@@ -7,74 +7,14 @@ import os
 from time import sleep
 import RPi.GPIO as GPIO
 
+
 #constants------------------------------------------------------------------------
 
-USE_GRAPH = 1
+# Switch levels for brightness sensor in lux
+LUX_LEVEL = [0.1, 0.5, 1, 20, 50, 100, 150, 200, 300, 400]
 
-if USE_GRAPH == 0:
-
-    # Switch levels for brightness sensor in lux
-    LUX_LEVEL = [0.1, 0.5, 1, 20, 50, 100, 150, 200, 300, 400]
-
-    # Set this display brightness by switch levels
-    DISP_BRIGHTNESS = [10, 30, 50, 70, 90, 120, 150, 180, 205, 230, 255]
-
-else:
-
-    STEP_COUNT = 10
-
-    def getGraph(GRP_CUT,GRP_PROP):    
-        tempGraph = []
-        NEW_STEP=GRP_CUT[0]/(STEP_COUNT*GRP_PROP[0])
-        for g in GRP_CUT:
-            grpIndex = GRP_CUT.index(g)
-            grpStep = (STEP_COUNT*GRP_PROP[grpIndex])    
-            stepCount = 0
-            while NEW_STEP < g:
-                tempGraph.append(NEW_STEP)
-                print(NEW_STEP)
-                if grpIndex > 1:
-                    NEXT_VAL = NEW_STEP+((GRP_CUT[grpIndex]-GRP_CUT[grpIndex-1])/grpStep)                
-                else:
-                    NEXT_VAL = NEW_STEP+(GRP_CUT[grpIndex]/grpStep)
-                if stepCount <= grpStep:
-                    NEW_STEP = NEXT_VAL
-                    stepCount+=1            
-            if grpIndex == len(GRP_CUT)-1:
-                NEW_STEP = GRP_CUT[grpIndex]
-                tempGraph.append(NEW_STEP)
-                print(NEW_STEP)
-        return tempGraph
-
-    #lux graph values
-    #0-1        3   20%
-    #2-50       2   20%
-    #51-200     3   30%
-    #201-400    2   20%
-
-    #set upper boundaries for groups
-    LUX_CUT = [1,50,200,400]
-
-    #set % of step count in each group
-    LUX_PROP = [0.3, 0.2, 0.3, 0.2]
-
-    #get lux graph for group values
-    LUX_LEVEL = getGraph(LUX_CUT,LUX_PROP)
-
-    #display brightness graph values
-    #0-9        1
-    #10-89      4
-    #90-179     3
-    #180-255    3
-
-    #set upper boundaries for groups
-    DISP_CUT = [10,75,180,255]      #adjust these numbers up or down to raise or lower brightness for each group
-
-    #set % of step count in each group
-    DISP_PROP = [0.3, 0.3, 0.2, 0.2]    #distribute towards top to raise brightness, towards bottom to lower brightness
-
-    #get lux graph for group values
-    DISP_BRIGHTNESS = getGraph(DISP_CUT,DISP_PROP)
+# Set this display brightness by switch levels
+DISP_BRIGHTNESS = [10, 30, 50, 70, 90, 120, 150, 180, 205, 230, 255]
 
 
 #day/night switching--------------------------------------------------------------
@@ -142,34 +82,46 @@ def getLux():
 
     #round lux value
     luxRounded = round(Lux,1)
+    
+    #check if we have any stored values
+    if len(READ_VALUES) > 0:
 
-    #check if lux value has changed - if yes, delete oldest reading and add new one
-    if READ_VALUES[len(READ_VALUES)] != luxRounded:
-
-        #make sure we've got a full set of readings to average over
-        if len(READ_VALUES) < AVG_COUNT:
-            #if so, delete oldest reading (otherwise, )
-            READ_VALUES.pop(0)
-
-        #add new one
-        READ_VALUES.append(luxRounded)
-
-        print ("Lux = {}\n".format(luxRounded))
-        os.system("echo {} > /tmp/tsl2561".format(luxRounded))
+        #check if lux value has changed - if yes, delete oldest reading and add new one
+        if READ_VALUES[len(READ_VALUES)-1] != luxRounded:
         
+            #make sure we've got a full set of readings to average over
+            if len(READ_VALUES) == AVG_COUNT:
+        
+                #if so, delete oldest reading (otherwise, wlet it ork up to avg_count)
+                READ_VALUES.pop(0)
+            
+            #add new reading
+            appendValue(luxRounded)
+
+    else:
+
+        #add new reading
+        appendValue(luxRounded)
+
     #return average of stored readings
     return sum(READ_VALUES)/len(READ_VALUES)
 
+def appendValue(luxValue):
+    
+    #add new lux value
+    READ_VALUES.append(luxValue)
+
+    print ("Lux = {}\n".format(luxValue))
+    os.system("echo {} > /tmp/tsl2561".format(luxValue))
+
 # Function for calculating brightness level and step
-
 def getBrightness(luxValue):
-
     for int in LUX_LEVEL:
         if int == 0:
             if luxValue < LUX_LEVEL[int]:
                 ret=int
                 break
-        elif int == len(LUX_LEVEL):
+        elif int == len(LUX_LEVEL)-1:
             if luxValue > LUX_LEVEL[int]:
                 ret=int
                 break
@@ -177,7 +129,6 @@ def getBrightness(luxValue):
             if luxValue >= LUX_LEVEL[int-1] and luxValue < LUX_LEVEL[int]:
                 ret=int
                 break
-
     return ret
 
 
