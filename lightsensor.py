f7d72067ec90660fe9a6e613d90e835e2fba88fd
jnnfrcbb <89925918+jnnfrcbb@users.noreply.github.com>
2023-12-05 18:23:22 +0000

    e

--- lightsensor.py
+++ lightsensor.py
@@ -126,7 +126,7 @@ def writeBrightness(NEW_BRIGHT):
 
 
 print (getLux)
-NEW_LEVEL = getLux 
+#NEW_LEVEL = getLux()
 
 #    #NEW_LEVEL = round(.0255*(((getLux()/400)*100)**2),2)
 
@@ -134,7 +134,7 @@ NEW_LEVEL = getLux
     
     #if (NEW_LEVEL > BRIGHT_LEVEL + 5) and (NEW_LEVEL < BRIGHT_LEVEL - 5):
 #    if NEW_LEVEL != BRIGHT_LEVEL:
-writeBrightness(NEW_LEVEL)
+#writeBrightness(NEW_LEVEL)
 #        BRIGHT_LEVEL = NEW_LEVEL
 
 sleep(AVG_TIME/AVG_COUNT)
