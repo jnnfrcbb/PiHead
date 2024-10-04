a2826398b249e954277c14e70e74907677ddcf0a
jnnfrcbb <89925918+jnnfrcbb@users.noreply.github.com>
2024-10-04 15:30:20 +0100

    e

--- tempsensor.py
+++ tempsensor.py
@@ -5,9 +5,9 @@ import time
 os.system("modprobe w1-gpio")
 os.system("modprobe w1-therm")
 
-base_dir = "/sys/bus/w1/devices"
+base_dir = "/sys/bus/w1/devices/"
 device_folder = glob.glob(base_dir + "28*")[0]
-device_file = device_folder + "/w1-slave"
+device_file = device_folder + "/w1_slave"
 
 def read_temp_raw():
     f = open(device_file, "r")
