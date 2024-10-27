91e25494741d8159ce0c2f2e829f64e0ed45285f
jnnfrcbb <89925918+jnnfrcbb@users.noreply.github.com>
2024-10-27 18:51:52 +0000

    e

--- fan_control.py
+++ fan_control.py
@@ -21,7 +21,6 @@ fan.start(defSpeed)
 
 def get_temp():                             # Function to read in the CPU temperature and return it as a float in degrees celcius
     output = open('/sys/class/thermal/thermal_zone0/temp', 'r')
-    temp_str = int(output)/1000
     output.close()
 
     return defSpeed
